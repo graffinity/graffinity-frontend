785a57cbbdc02272ff4a51f851d4d9cc51ce432e
KerniusSur <kernius.survila@gmail.com>
2022-10-28 17:44:20 +0300

    GRAF-92 fix after merge

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,17 +1,9 @@
 import "./AppRouter.css";
 
-<<<<<<< HEAD
 import MultiActionAreaCard from "components/common/Card";
 import Header from "components/common/Header";
 import Map from "components/map/Map";
 import { FooterContainer } from "Containers/footerContainer";
-=======
-import HomePage from "pages/home/HomePage";
-import ResponsiveAppBar from "components/common/Header";
-import Map from "components/Map/Map";
-import { FooterContainer } from "Containers/footerContainer";
-import MultiActionAreaCard from "components/common/Card";
->>>>>>> 74b64bbb1e8b95f27d7eae87cfb887331bde6692
 
 const AppRouter = () => {
 	return (
@@ -22,17 +14,10 @@ const AppRouter = () => {
 				display: "flex",
 				flexDirection: "column",
 			}}>
-<<<<<<< HEAD
 			{/* <HomePage /> */}
 			<>
 				<div className='bg-primary'>
 					<Header />
-=======
-			<HomePage />
-			<>
-				<div className='bg-primary'>
-					<ResponsiveAppBar />
->>>>>>> 74b64bbb1e8b95f27d7eae87cfb887331bde6692
 					<div className='row g-0 bg-transparent '>
 						<div className='col bg-transparent d-flex justify-content-center '>
 							<div className='leftSide'>
