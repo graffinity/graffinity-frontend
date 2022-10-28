3f30f0e6f908d3f1caf7ee2ba87d8886e76f794a
KerniusSur <kernius.survila@gmail.com>
2022-10-28 16:52:12 +0300

    GRAF-29 added AppTheme

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,21 @@
-import React from "react";
+import { ThemeProvider } from "@emotion/react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./AppRouter";
+import AppTheme from "./AppTheme";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+	document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <AppRouter />
-    </React.StrictMode>
-  </BrowserRouter>
+	<BrowserRouter>
+		<ThemeProvider theme={AppTheme}>
+			<AppRouter />
+		</ThemeProvider>
+	</BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
