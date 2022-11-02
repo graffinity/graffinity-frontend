eb3e889b341b75999cd190f7ce1a376fa7b2decb
KerniusSur <74358398+KerniusSur@users.noreply.github.com>
2022-11-02 23:19:58 +0200

    Graf 31 front end developing the drawer section on mobile version (#20)
    * Added contents to the drawer
    
    * GRAF-31 ran lint
    
    * forgot to format
    
    Co-authored-by: Dominykas3000 <dominykas.svilpa@mif.stud.vu.lt>

--- src/components/map/MapComponent.tsx
+++ src/components/map/MapComponent.tsx
@@ -5,78 +5,77 @@ import React, { useState } from "react";
 import "./Map.css";
 
 const center = {
-	lat: 54.730012,
-	lng: 25.262638,
+  lat: 54.730012,
+  lng: 25.262638,
 };
 
 const center2 = {
-	lat: 25.262648,
-	lng: 54.730012,
+  lat: 25.262648,
+  lng: 54.730012,
 };
 
 interface MapComponentProps {
-	width: number;
-	height: number;
+  width: number;
+  height: number;
 }
 
 const MapComponent = (props: MapComponentProps) => {
-	const { isLoaded } = useJsApiLoader({
-		id: "google-map-script",
-		googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-	});
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
 
-	const [map, setMap] = useState<google.maps.Map | null>(null);
+  const [map, setMap] = useState<google.maps.Map | null>(null);
 
-	const onLoad = React.useCallback(function callback(map: google.maps.Map) {
-		const bounds = new window.google.maps.LatLngBounds(center);
-		map.fitBounds(bounds);
-		setMap(map);
-	}, []);
+  const onLoad = React.useCallback(function callback(map: google.maps.Map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
 
-	const onUnmount = React.useCallback(function callback(
-		map: google.maps.Map
-	) {
-		setMap(null);
-	},
-		[]);
+  const onUnmount = React.useCallback(function callback(map: google.maps.Map) {
+    setMap(null);
+  }, []);
 
-	return (
-		<div
-			style={{
-				width: "100%",
-				height: "100%",
-				display: "flex",
-				flexDirection: "column",
-			}}>
-			<div
-				style={{
-					width: "100%",
-					display: "flex",
-					justifyContent: "center",
-					marginTop: "16px",
-					marginBottom: "16px",
-					marginRight: "32px",
-				}}>
-				{isLoaded ? (
-					<GoogleMap
-						mapContainerStyle={{
-							width: props.width,
-							height: props.height,
-
-						}}
-						center={center}
-						zoom={10}
-						onLoad={onLoad}
-						onUnmount={onUnmount}>
-						{/* <Marker position={center2} />
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "16px",
+          marginBottom: "16px",
+          marginRight: "32px",
+        }}
+      >
+        {isLoaded ? (
+          <GoogleMap
+            mapContainerStyle={{
+              width: props.width,
+              height: props.height,
+            }}
+            center={center}
+            zoom={10}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+          >
+            {/* <Marker position={center2} />
             <Marker position={center} /> */}
-					</GoogleMap>
-				) : (
-					<></>
-				)}
-			</div>
-		</div>
-	);
+          </GoogleMap>
+        ) : (
+          <></>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default MapComponent;
