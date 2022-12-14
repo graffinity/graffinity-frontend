22cba7705a99cdac588ad8718291bf70a7dc9ed3
KerniusSur <kernius.survila@gmail.com>
2022-12-15 01:50:35 +0200

    dep update + map compnent update

--- src/components/map/MapComponent.tsx
+++ src/components/map/MapComponent.tsx
@@ -6,18 +6,17 @@ import {
 	Marker,
 	useJsApiLoader,
 } from "@react-google-maps/api";
-import React, { useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import usePlacesAutocomplete, {
 	getGeocode,
 	getLatLng,
 } from "use-places-autocomplete";
 
+import { IconButton } from "@mui/material";
 import { formatRelative } from "date-fns";
+import { ReactComponent as CompassIcon } from "./compass.svg";
 import "./Map.css";
 import mapStyles from "./mapStyles";
-import { ReactComponent as CompassIcon } from "./compass.svg";
-import { ReactComponent as LocationIcon } from "./location.svg";
-import { IconButton } from "@mui/material";
 
 interface MarkerData {
 	lat: number;
@@ -47,131 +46,125 @@ export default function MapComponent(props: MapComponentProps) {
 		libraries: ["places"],
 	});
 
-	const [markers, setMarkers] = React.useState<MarkerData[]>([]);
+	const [markers, setMarkers] = useState<MarkerData[]>([]);
 	const [map, setMap] = useState<google.maps.Map | null>(null);
-	const [selected, setSelected] = React.useState<MarkerData | null>(null);
-	const mapRef = React.useRef<google.maps.Map | null>(null);
+	const [selected, setSelected] = useState<MarkerData | null>(null);
+	const mapRef = useRef<google.maps.Map | null>(null);
 
-	const onLoad = React.useCallback(function callback(map: google.maps.Map) {
+	const onUnmount = useCallback(function callback(map: google.maps.Map) {
+		setMap(null);
+	}, []);
+	const onMapClick = useCallback((event: google.maps.MapMouseEvent) => {
+		if (event.latLng) {
+			let newMarker: MarkerData = {
+				lat: event.latLng.lat(),
+				lng: event.latLng.lng(),
+				time: new Date(),
+			};
+			setMarkers((previous) => [...previous, newMarker]);
+		}
+	}, []);
+
+	const onMapLoad = useCallback((map: google.maps.Map) => {
+		mapRef.current = map;
 		const bounds = new window.google.maps.LatLngBounds(center);
 		map.fitBounds(bounds);
 		setMap(map);
 	}, []);
 
-	const onUnmount = React.useCallback(function callback(map: google.maps.Map) {
-		setMap(null);
-	}, []);
-	const onMapClick = React.useCallback((e) => {
-		setMarkers((previous) => [
-			...previous,
-			{
-				lat: e.latLng.lat(),
-				lng: e.latLng.lng(),
-				time: new Date(),
-			},
-		]);
-	}, []);
-
-	// const mapRef = React.useRef();
-	const onMapLoad = React.useCallback((map: google.maps.Map) => {
-		mapRef.current = map;
-	}, []);
-
-	const panTo = React.useCallback(({ lat, lng }) => {
-		// if (mapRef.current) {
-		// 	setMap(mapRef.current.panTo({ lat, lng }))
-		// }
-		mapRef.current?.panTo({ lat, lng });
-		mapRef.current?.setZoom(18);
+	const panTo = useCallback((coords: google.maps.LatLngLiteral) => {
+		if (mapRef.current) {
+			mapRef.current?.panTo(coords);
+			mapRef.current?.setZoom(18);
+			setMap(mapRef.current);
+		}
 	}, []);
 
 	return (
-		<>
-			<div
-				className="map-container"
-				style={{
-					width: "100%",
-					flex: 1,
-					display: "flex",
-					flexDirection: "column",
-				}}
-			>
-				{/* <Search panTo={panTo} /> */}
-				<Locate panTo={panTo} />
+		<div
+			className="map-container"
+			style={{
+				width: "100%",
+				flex: 1,
+				display: "flex",
+				flexDirection: "column",
+			}}
+		>
+			<Search panTo={panTo} />
+			<Locate panTo={panTo} />
 
-				{isLoaded ? (
-					<>
-						<GoogleMap
-							mapContainerClassName="map"
-							mapContainerStyle={{
-								width:
-									props.width > maxWidthForDesktopView
-										? `calc(${props.width}px /2)`
-										: "100%",
-								// height: props.height ? props.height : "100%",
-								height: props.height ? props.height : "20%",
+			{isLoaded && (
+				<GoogleMap
+					mapContainerClassName="map"
+					mapContainerStyle={{
+						width:
+							props.width > maxWidthForDesktopView
+								? `calc(${props.width}px /2)`
+								: "100%",
+						height: props.height ? props.height : "80%",
+					}}
+					center={center}
+					options={options}
+					onClick={onMapClick}
+					onLoad={onMapLoad}
+					onUnmount={onUnmount}
+				>
+					{markers.map((marker) => (
+						<Marker
+							key={marker.time.toISOString()}
+							position={{
+								lat: marker.lat,
+								lng: marker.lng,
 							}}
-							zoom={18}
-							center={center}
-							options={options}
-							onClick={onMapClick}
-							onLoad={onMapLoad}
-							onUnmount={onUnmount}
+							// icon={{
+							// 	url: './location.svg',
+							// 	origin: new window.google.maps.Point(0, 0),
+							// 	anchor: new window.google.maps.Point(15, 15),
+							// 	scaledSize: new window.google.maps.Size(30, 30),
+							// }}
+							onClick={() => {
+								setSelected(marker);
+							}}
+						/>
+					))}
+					{selected && (
+						<InfoWindow
+							position={{
+								lat: selected.lat,
+								lng: selected.lng,
+							}}
+							onCloseClick={() => {
+								setSelected(null);
+							}}
 						>
-							{markers.map((marker) => (
-								<Marker
-									key={marker.time.toISOString()}
-									position={{
-										lat: marker.lat,
-										lng: marker.lng,
-									}}
-									// icon={{
-									// 	url: './location.svg',
-									// 	origin: new window.google.maps.Point(0, 0),
-									// 	anchor: new window.google.maps.Point(15, 15),
-									// 	scaledSize: new window.google.maps.Size(30, 30),
-									// }}
-									onClick={() => {
-										setSelected(marker);
-									}}
-								/>
-							))}
-							{selected ? (
-								<InfoWindow
-									position={{
-										lat: selected.lat,
-										lng: selected.lng,
-									}}
-									onCloseClick={() => {
-										setSelected(null);
-									}}
-								>
-									<div>
-										<p>Graffiti spotted watch out !</p>
-										<p>Spotted {formatRelative(selected.time, new Date())}</p>
-									</div>
-								</InfoWindow>
-							) : null}
-						</GoogleMap>
-					</>
-				) : (
-					<></>
-				)}
-			</div>
-		</>
+							<div>
+								<p>Graffiti spotted watch out !</p>
+								<p>Spotted {formatRelative(selected.time, new Date())}</p>
+							</div>
+						</InfoWindow>
+					)}
+				</GoogleMap>
+			)}
+		</div>
 	);
 }
-function Locate({ panTo }) {
+
+interface LocateAndSearchProps {
+	panTo: (coords: google.maps.LatLngLiteral) => void;
+}
+function Locate(props: LocateAndSearchProps) {
+	const { panTo } = props;
 	return (
 		<IconButton
 			className="locate"
 			onClick={() => {
 				navigator.geolocation.getCurrentPosition(
 					(position) => {
-						panTo({
+						let coords: google.maps.LatLngLiteral = {
 							lat: position.coords.latitude,
 							lng: position.coords.longitude,
-						});
+						};
+						panTo(coords);
 					},
 					() => null
 				);
@@ -187,12 +180,8 @@ function Locate({ panTo }) {
 	);
 }
 
-function Search({ panTo }) {
-	// let newLocation: google.maps.LatLng = {
-
-	// 	lat: () => 0,
-	// 	lng: () => 0
-	// }
+function Search(props: LocateAndSearchProps) {
+	const { panTo } = props;
 	const {
 		ready,
 		value,
@@ -217,13 +206,14 @@ function Search({ panTo }) {
 		clearSuggestions();
 
 		try {
-			const results = await getGeocode({ address });
-			const { lat, lng } = getLatLng(results[0]);
-			panTo({ lat, lng });
+			let results = await getGeocode({ address });
+			let coords = getLatLng(results[0]);
+			panTo(coords);
 		} catch (error) {
 			console.log("😱 Error: ", error);
 		}
 	};
+	return <div>search</div>;
 
 	// return (
 	// 	<div className='search'>
