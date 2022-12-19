975dd78f00cc1e637f8881192bcc595b08ae0647
KerniusSur <kernius.survila@gmail.com>
2022-12-19 18:09:46 +0200

    GRAF-68 fix eslint warnings

--- src/api/GraffitiPhotoAPI.ts
+++ src/api/GraffitiPhotoAPI.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 import apiEndpoints from "constants/apiEndpoints";
-import GraffitiPhotoRequest from "models/graffitiphoto/GraffitiPhotoRequest";
-import GraffitiPhotoResponse from "models/graffitiphoto/GraffitiPhotoResponse";
 
 const baseUrl = apiEndpoints.graffitiPhoto;
 
--- src/pages/sandbox/Sandbox.tsx
+++ src/pages/sandbox/Sandbox.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Button } from "@mui/material";
 import GraffitiPhotoAPI from "api/GraffitiPhotoAPI";
 import GraffitiPostAPI from "api/GraffitiPostAPI";
