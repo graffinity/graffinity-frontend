d2c40ed8662d2b6b7139a06745809bd2d6e472e9
KerniusSur <kernius.survila@gmail.com>
2023-01-19 05:05:58 +0200

    GRAF-82 build script fix

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Login to AWS ECR ; Build and push the docker image to AWS ECR
 aws ecr get-login-password | docker login --username AWS --password-stdin 445007777844.dkr.ecr.eu-central-1.amazonaws.com
-docker buildx build --load -f Dockerfile . -t 445007777844.dkr.ecr.eu-central-1.amazonaws.com/graffinity-frontend
+docker build . -t 445007777844.dkr.ecr.eu-central-1.amazonaws.com/graffinity-frontend --platform linux/amd64
 docker push 445007777844.dkr.ecr.eu-central-1.amazonaws.com/graffinity-frontend
