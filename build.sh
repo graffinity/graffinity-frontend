980fb4ae2050285c62021b2a9dfd1a26c15e57bc
KerniusSur <kernius.survila@gmail.com>
2023-01-19 05:04:40 +0200

    GRAF-82 minor ci changes

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 # Build the docker image
-docker buildx build --load -f Dockerfile . -t 445007777844.dkr.ecr.eu-central-1.amazonaws.com/graffinity-frontend
+docker build . -t 445007777844.dkr.ecr.eu-central-1.amazonaws.com/graffinity-frontend --platform linux/amd64
