c3c0d72457fe832354659f37efc6993bed0fcb12
KerniusSur <kernius.survila@gmail.com>
2023-01-20 00:36:34 +0200

    other changes

--- src/pages/GraffitiFullView/GraffitiFullView.tsx
+++ src/pages/GraffitiFullView/GraffitiFullView.tsx
@@ -2,8 +2,18 @@ import SwipeableTextMobileStepper from "components/common/ImageSlider";
 import LikeButton from "components/common/LikeButton";
 import React from "react";
 import "./GraffitiFulView.css";
+import { useParams } from "react-router-dom";
+import GraffitiPostAPI from "api/GraffitiPostAPI";
 
 export default function GraffitiFullView() {
+	const { id } = useParams<{ id: string }>();
+
+	// const getPictures = async () => {
+	// 	if (id) {
+	// 		const graffiti = await GraffitiPostAPI.findById(+id);
+	// 	}
+	// };
+
 	return (
 		<div className="PeperStepper">
 			<SwipeableTextMobileStepper />
