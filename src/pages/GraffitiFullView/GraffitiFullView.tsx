538c5581f08191f759c36032f7e20b3062cef072
KerniusSur <kernius.survila@gmail.com>
2023-01-21 02:35:56 +0200

    GRAF-83 eslint fixes

--- src/pages/GraffitiFullView/GraffitiFullView.tsx
+++ src/pages/GraffitiFullView/GraffitiFullView.tsx
@@ -1,13 +1,8 @@
 import SwipeableTextMobileStepper from "components/common/ImageSlider";
 import LikeButton from "components/common/LikeButton";
-import React from "react";
 import "./GraffitiFulView.css";
-import { useParams } from "react-router-dom";
-import GraffitiPostAPI from "api/GraffitiPostAPI";
 
 export default function GraffitiFullView() {
-	const { id } = useParams<{ id: string }>();
-
 	// const getPictures = async () => {
 	// 	if (id) {
 	// 		const graffiti = await GraffitiPostAPI.findById(+id);
