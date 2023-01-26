33f988f03ef91674aaae4ad58cf0b0fd9c486eb3
Dominykas3000 <dominykas.svilpa@mif.stud.vu.lt>
2023-01-26 13:22:43 +0200

    ran lint

--- src/components/common/FullViewDesc.tsx
+++ src/components/common/FullViewDesc.tsx
@@ -1,78 +1,90 @@
 import { Typography } from "@mui/material";
+import { useJsApiLoader } from "@react-google-maps/api";
+import googleDefaultConfig from "constants/GoogleConfig";
 import GraffitiResponse from "models/graffiti/GraffitiResponse";
 import moment from "moment";
-import React, { useEffect, useState } from 'react';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, { useEffect, useState } from "react";
 import { getAddress } from "utils/LocationUtil";
 interface DescriptionProps {
-    graffiti: GraffitiResponse;
+	graffiti: GraffitiResponse;
 }
 
 export default function Description(props: DescriptionProps) {
-    const { graffiti } = props;
-    const geoCoder = new google.maps.Geocoder();
-    const [address, setAddress] = useState<string>();
+	const { graffiti } = props;
+	let { isLoaded } = useJsApiLoader({ ...googleDefaultConfig });
+	const [address, setAddress] = useState<string>();
 
-    async function getGraffitiAddress() {
-        const lat = graffiti.latitude.toString();
-        const lng = graffiti.longitude.toString();
+	async function getGraffitiAddress() {
+		const lat = graffiti.latitude.toString();
+		const lng = graffiti.longitude.toString();
+		const geoCoder = new google.maps.Geocoder();
+		let address = await getAddress(geoCoder, lat, lng);
+		setAddress(address);
+	}
 
-        let address = await getAddress(geoCoder, lat, lng)
-        setAddress(address)
-    }
+	useEffect(() => {
+		const google = window.google;
 
-    useEffect(() => {
-        getGraffitiAddress();
-    }, [])
+		getGraffitiAddress();
+	}, []);
 
-    return (
-        <div
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                width: "100%",
-                marginTop: "64px",
-                marginRight: "16px",
-                border: "1px solid #FFFFFF",
-                boxSizing: "border-box",
-                borderRadius: "16px",
-            }}
-        >
-            <Typography
-                variant="h2"
-                style={{
-                    color: "white",
-                    marginBottom: "16px",
-                    marginTop: "16px"
-                }}
-            >Name of the Graffiti:{graffiti.name}
-            </Typography>
-            <Typography
-                variant="h3"
-                style={{
-                    color: "white",
-                    marginBottom: "16px",
-                    marginTop: "16px"
-                }}
-            >{address}</Typography>
+	return (
+		<div
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				width: "100%",
+				marginTop: "64px",
+				marginRight: "16px",
+				border: "1px solid #FFFFFF",
+				boxSizing: "border-box",
+				borderRadius: "16px",
+			}}
+		>
+			<Typography
+				variant="h2"
+				style={{
+					color: "white",
+					marginBottom: "16px",
+					marginTop: "16px",
+				}}
+			>
+				Name of the Graffiti:{graffiti.name}
+			</Typography>
+			{address && (
+				<Typography
+					variant="h3"
+					style={{
+						color: "white",
+						marginBottom: "16px",
+						marginTop: "16px",
+					}}
+				>
+					{address}
+				</Typography>
+			)}
 
-            <Typography
-                variant="h4"
-                style={{
-                    color: "white",
-                    marginBottom: "16px",
-                    marginTop: "16px"
-                }}
-            >{graffiti.description}</Typography>
-            <Typography variant="h5"
-                style={{
-                    color: "white",
-                    marginBottom: "16px",
-                    marginTop: "16px"
-                }}
-            >
-                Created At:{moment(graffiti.creationDate).format("YYYY/MM/DD")}</Typography>
-        </div>
-    );
+			<Typography
+				variant="h4"
+				style={{
+					color: "white",
+					marginBottom: "16px",
+					marginTop: "16px",
+				}}
+			>
+				{graffiti.description}
+			</Typography>
+			<Typography
+				variant="h5"
+				style={{
+					color: "white",
+					marginBottom: "16px",
+					marginTop: "16px",
+				}}
+			>
+				Created At:{moment(graffiti.creationDate).format("YYYY/MM/DD")}
+			</Typography>
+		</div>
+	);
 }
