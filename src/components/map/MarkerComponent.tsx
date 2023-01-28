9ea33176279d1f164a43c74164257f14604289bc
KerniusSur <kernius.survila@gmail.com>
2023-01-28 12:28:24 +0200

    GRAF-91 marker infowindow image 'slider'

--- src/components/map/MarkerComponent.tsx
+++ src/components/map/MarkerComponent.tsx
@@ -1,6 +1,8 @@
-import { Box, Typography } from "@mui/material";
+import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
+import { IconButton, Typography } from "@mui/material";
 import { InfoWindow, Marker } from "@react-google-maps/api";
 import MarkerData from "models/map/MarkerData";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 interface MarkerComponentProps {
@@ -24,6 +26,23 @@ const MarkerComponent = (props: MarkerComponentProps) => {
 		handleActiveMarkerNull,
 	} = props;
 
+	const images = marker.photos.map((photo) => photo.url);
+	const maxSteps = images.length;
+	const [currentImage, setCurrentImage] = useState<string>(images[0]);
+	const [activeStep, setActiveStep] = useState<number>(0);
+
+	const imageRef = useRef<HTMLImageElement | null>(null);
+
+	const handleNext = () => {
+		setCurrentImage(images[activeStep + 1]);
+		setActiveStep((prevActiveStep) => prevActiveStep + 1);
+	};
+
+	const handleBack = () => {
+		setCurrentImage(images[activeStep - 1]);
+		setActiveStep((prevActiveStep) => prevActiveStep - 1);
+	};
+
 	const navigate = useNavigate();
 	return (
 		<Marker
@@ -33,7 +52,11 @@ const MarkerComponent = (props: MarkerComponentProps) => {
 		>
 			{activeMarker?.id === marker.id && (
 				<InfoWindow
-					options={{}}
+					options={{
+						maxWidth: imageRef.current?.clientWidth
+							? imageRef.current?.clientWidth
+							: undefined,
+					}}
 					ref={infoRef}
 					onLoad={(infoWindow) => {
 						let infoWindowElement = infoWindow.getContent() as HTMLElement;
@@ -43,32 +66,57 @@ const MarkerComponent = (props: MarkerComponentProps) => {
 					onCloseClick={() => handleActiveMarkerNull()}
 				>
 					<div
-						ref={imgContainerRef}
 						style={{
 							display: "flex",
-							flexDirection: "column",
 							alignItems: "center",
-							gap: "8px",
 							width: "100%",
-							padding: "16px",
-							boxSizing: "border-box",
 						}}
 					>
-						<Typography variant="body2">{marker.name}</Typography>
-						{marker.photos.length !== 0 && (
-							<Box
-								component="img"
-								src={marker.photos[0].url}
-								style={{
-									maxWidth: "100%",
-									maxHeight: "350px",
-								}}
-								sx={{ ":hover": { cursor: "pointer", opacity: "0.8" } }}
-								onClick={() => {
-									navigate(`/graffiti/view/${marker.id}`);
-								}}
-							/>
-						)}
+						<IconButton onClick={handleBack} disabled={activeStep === 0}>
+							<KeyboardArrowLeft />
+						</IconButton>
+						<div
+							ref={imgContainerRef}
+							style={{
+								display: "flex",
+								flexDirection: "column",
+								alignItems: "center",
+								gap: "8px",
+								// width: "100%",
+								padding: "16px",
+								height: undefined,
+								aspectRatio: 1 / 1,
+								// boxSizing: "border-box",
+							}}
+						>
+							<Typography variant="body2">{marker.name}</Typography>
+							{marker.photos.length !== 0 && (
+								<img
+									alt="GraffitiImage"
+									ref={imageRef}
+									src={currentImage}
+									style={{
+										maxWidth: "100%",
+										// width: "min-content",
+										// maxHeight: "350px",
+										height: undefined,
+										aspectRatio: 1 / 1,
+
+										cursor: "pointer",
+										opacity: "0.8",
+									}}
+									onClick={() => {
+										navigate(`/graffiti/view/${marker.id}`);
+									}}
+								/>
+							)}
+						</div>
+						<IconButton
+							onClick={handleNext}
+							disabled={activeStep === maxSteps - 1}
+						>
+							<KeyboardArrowRight />
+						</IconButton>
 					</div>
 				</InfoWindow>
 			)}
